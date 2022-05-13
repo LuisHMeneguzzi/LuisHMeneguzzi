e48fa8da1168fe4376ae36742ca97c3f65c3409d
LuisHMeneguzzi <luishpmeneguzzi@gmail.com>
2022-05-12 22:05:33 -0300

    Create Exemplosstr.py

---
+++ Aula7/Aula8/Exemplosstr.py
@@ -0,0 +1,16 @@
+frase = 'Ola mundo'
+for letra in frase:#letra vai assumir os valores da string
+    print(letra)
+print('-----------')
+for i in range(len(frase)):#i será os indices de 0 a tam-1
+    print(frase[i])
+
+
+texto = 'Uva, Pera e banana'
+
+print(texto[5:9])
+print(texto[12:])
+print(texto[:3])
+print('----------')
+
+texto = 'Uva, Pera e banana'
